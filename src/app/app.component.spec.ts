c37d162118868489941b1e0a2174507604957bd8
Sandro Böhme <sandro.boehme@gmx.de>
2020-01-27 20:09:39 +0100

    Added GitHub Pages deployment; Removed not yet working component; Fixed typos

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,6 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'ng-uikit-pro-standard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng-uikit-pro-standard');
+    expect(app.title).toEqual('NgUikitProStandard');
   });
 });
--- src/app/parallax/bb-plx-img-block.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "bb-plx-img-block",
-  templateUrl: "./bb-plx-img-block.component.html",
-  styleUrls: ["./bb-plx-img-block.component.scss"]
-})
-export class AppComponent {}
